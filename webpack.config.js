8c88c45e2ba5c70efc68db4dd411f144feeb3e74
sdplion <iigresmorfmus@gmail.com>
2020-11-08 20:42:12 +0200

    edit config htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,26 @@ module.exports = {
     entry: './src/index.js', // входная точка - исходный файл
     plugins: [
         new HtmlWebpackPlugin({
-            title: '3trw',
-            favicon: 'favicon.ico'
+            //title: '3trw',
+            favicon: './src/favicon.ico',
+            minify: false,
+            templateContent: `<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>3trw</title>     
+</head>            
+<body>
+     <noscript>You need to enable JavaScript to run this app.</noscript>
+     <div id="root"></div>
+</body>         
+</html>`
+
         }),
     ],
     output: {
         path: path.resolve(__dirname, './dist'),     // путь к каталогу выходных файлов - папка dist
-        publicPath: '/dist/',
+        //publicPath: '/dist/',
         filename: "bundle.js"       // название создаваемого файла
     },
     module: {
